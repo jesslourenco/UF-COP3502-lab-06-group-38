a23780f91c54514ffeffa1d77fbea3d981645357
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2023-02-27 21:46:10 -0500

    lab06: main function

--- password_encoder_decoder.py
+++ password_encoder_decoder.py
@@ -1,7 +1,7 @@
 # Student name: Jessica Lourenco
 
 def encode():
-    pass
+    
 
 def print_menu():
     print('Menu \n-------------')
@@ -11,7 +11,25 @@ def print_menu():
 
 
 def main():
-    pass
+    option = 0
+    encrypted_pwrd = ''
 
+    while option != 3:
+        print_menu()
+        option = int(input('Please enter an option: '))
+
+        if option == 1:
+            pwrd = input('Please enter your password to encode: ')
+            encrypted_pwrd = encode(pwrd)
+            print('Your password has been encoded and stored!')
+
+        if option == 2:
+            pass
+
+        if option == 3:
+            break
+
+    exit()
+        
 if __name__ == '__main__':
     main()
